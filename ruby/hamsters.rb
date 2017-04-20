3497781fb6cc8af22850bee811117970161cc42c
Jim Kyle <jim@jwkyle.com>
2017-04-20 11:15:41 -0500

    Add r1 info

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,1 +1,29 @@
-# Hamster Name, Squeak level
+# Hamster Name, Squeak level, Fur Color, Good candidate for adoption?, est. age
+
+puts "What's the hamster's name?"
+name = gets.chomp
+puts "The hamster's name is #{name}."
+puts "How loud is the hamster from 0 (quiet) to 10 (loudest)?"
+loud = gets.chomp.to_i
+puts "The hamster is a #{loud} on the loud scale."
+puts "What color is the hamster?"
+color = gets.chomp
+puts "The hamster is #{color}."
+puts "Is the hamster a good candidate for adoption? (y/n)"
+adopt = gets.chomp 
+  if adopt == "y"
+    puts "The hamster is a good candidate for adoption."
+  elsif adopt == "n"
+    puts "The hamster is NOT a good candidate for adoption."
+  else
+    puts "please re-enter your answer as a y or n"
+  end
+puts "How old is #{name} in years?"
+age = gets.chomp.to_f
+  if age == 0.0
+    age = nil
+    puts "#{name}'s age is undetermined."  
+  else
+    puts "I have #{name} as #{age} years old."
+  end
+
