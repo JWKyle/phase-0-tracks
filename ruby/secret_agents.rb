fc57a9a4f2d5421c80076f53ac0d8fffe47a0696
Jim Kyle <jim@jwkyle.com>
2017-04-21 17:19:47 -0500

    Initial commit, start of coding

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,16 @@
+# Encrypt and Decrypt Text
+
+# Take every letter, advance one
+
+# assume lowercase inputs
+
+# ignore spaces
+
+def encrypt(x)
+  for i in 0...x.length
+    puts x[i]
+  end
+end
+encrypt("abcde")
+
+
