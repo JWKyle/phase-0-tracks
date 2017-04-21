2dc958875cd82232836fbfacb3379a54f53a33f8
Jim Kyle <jim@jwkyle.com>
2017-04-21 11:59:52 -0500

    add data gathering code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,1 +1,12 @@
 # Vampire Data collection
+
+puts "What is our name?"
+name = gets.chomp
+puts "Thanks!  Now, how old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birthyear = gets.chomp
+puts "Oh, by the way, our dining room serves some great garlic bread, however we need to order it ahead of time.  Would you like me to order some for you? (y or n)"
+bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insure = gets.chomp
