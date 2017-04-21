84c43dc0f26f4ac06e96aacdb63ac745010662e2
Jim Kyle <jim@jwkyle.com>
2017-04-21 15:51:43 -0500

    edit data collection

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,9 +6,11 @@ puts "Thanks!  Now, how old are you?"
 age = gets.chomp
 puts "What year were you born?"
 birthyear = gets.chomp
+birthage = birthyear - year.current
 puts "Our dining room serves some great garlic bread,"  
 puts "however we need to order it ahead of time." 
 puts "Would you like me to order some for you? (y or n)"
 bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance? (y or n)"
 insure = gets.chomp
+
