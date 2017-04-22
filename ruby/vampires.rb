95e6c6923163c528a86f82ea5a8b5ba07c68a800
Jim Kyle <jim@jwkyle.com>
2017-04-21 21:41:36 -0500

    end of night 042117

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,7 +7,6 @@ age = gets.chomp
 puts "What year were you born?"
 birthyear = gets.chomp
 birthage = Time.new.year - birthyear.to_i
-
 valid_input = false
 until valid_input
   puts "Our dining room serves some great garlic bread,"  
@@ -22,22 +21,22 @@ until valid_input
       valid_input = true
     else
       puts "please re-enter your answer as a 'y' or 'n' please."
-    end # Need to fix "n" answer
+    end 
 end
 valid_input2 = false
 until valid_input2
   puts "Would you like to enroll in the company's health insurance? (y or n)"
   insure = gets.chomp
     if insure == "y"
-      puts "I recorded your response as 'Yes.'"
+      puts "I have recorded your response as 'Yes.'"
       valid_input2 = true
     elsif insure == "n"
-      puts "I recorded your response as 'No.'"
+      puts "I have recorded your response as 'No.'"
       valid_input2 = true
     else
-      puts "Please select either 'y' or 'n' please."
+      puts "Select either 'y' or 'n' please."
     end
 end
+puts "Thank you for filling out this survey.  Welcome to Werewolf Inc!"
 
-
-(birthage == age) && ()
+(birthage == age.to_i) && ()
