23fd49aeb8788437181f95e06b9da2d5f1547754
Jim Kyle <jim@jwkyle.com>
2017-04-21 21:28:39 -0500

    add loop to insure

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,7 +6,8 @@ puts "Thanks!  Now, how old are you?"
 age = gets.chomp
 puts "What year were you born?"
 birthyear = gets.chomp
-birthage =  Time.new.year - birthyear
+birthage = Time.new.year - birthyear.to_i
+
 valid_input = false
 until valid_input
   puts "Our dining room serves some great garlic bread,"  
@@ -21,13 +22,22 @@ until valid_input
       valid_input = true
     else
       puts "please re-enter your answer as a 'y' or 'n' please."
+    end # Need to fix "n" answer
+end
+valid_input2 = false
+until valid_input2
+  puts "Would you like to enroll in the company's health insurance? (y or n)"
+  insure = gets.chomp
+    if insure == "y"
+      puts "I recorded your response as 'Yes.'"
+      valid_input2 = true
+    elsif insure == "n"
+      puts "I recorded your response as 'No.'"
+      valid_input2 = true
+    else
+      puts "Please select either 'y' or 'n' please."
     end
 end
-puts "Our dining room serves some great garlic bread,"  
-puts "however we need to order it ahead of time." 
-puts "Would you like me to order some for you? (y or n)"
-bread = gets.chomp
-puts "Would you like to enroll in the company's health insurance? (y or n)"
-insure = gets.chomp
+
 
 (birthage == age) && ()
