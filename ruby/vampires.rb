7c2bd5f98f2a577e4ca31077a0ea968478141062
Jim Kyle <jim@jwkyle.com>
2017-04-21 21:11:08 -0500

    add if loop for bread

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,6 +7,22 @@ age = gets.chomp
 puts "What year were you born?"
 birthyear = gets.chomp
 birthage =  Time.new.year - birthyear
+valid_input = false
+until valid_input
+  puts "Our dining room serves some great garlic bread,"  
+  puts "however we need to order it ahead of time." 
+  puts "Would you like me to order some for you? (y or n)"
+  bread = gets.chomp 
+    if  bread == "y"
+      puts "I'll put your order in right away."
+      valid_input = true
+    elsif bread == "n"
+      puts "OK, but I assure you, you're totally missing out."
+      valid_input = true
+    else
+      puts "please re-enter your answer as a 'y' or 'n' please."
+    end
+end
 puts "Our dining room serves some great garlic bread,"  
 puts "however we need to order it ahead of time." 
 puts "Would you like me to order some for you? (y or n)"
