e3bee71d4780f90951fdbb45532f104b53dd462e
Jim Kyle <jim@jwkyle.com>
2017-04-26 17:27:44 -0500

    Add final R2 problem, edit

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -15,18 +15,6 @@
 # puts "Thanks for playing!"
 
 
-#   puts "What's your dog's name?"
-#   dog = gets.chomp
-#   puts "What breed is he/she?"
-#   breed = gets.chomp
-
-# def dog_info(dog, breed)
-#   puts "Wow, #{dog} is a lovely #{breed}!"
-# end
-
-# dog_info( "#{dog}", "#{breed}")
-
-
 ## Release 1
 
 # Ninja Turtles
@@ -86,11 +74,69 @@ letters = {
   beta: "b",
   delta: "d",
   epsilon: "e",
-
 }
 
 numbers = [1, 2, 3, 4, 5]
 
-##
+## 1
 
+numbers.delete_if {|digit| digit < 3}
+p numbers
+
+letters.delete_if {|greek, letter| letter < "d" }
+p letters
+
+## 2
+
+letters = {
+  alpha: "a",
+  beta: "b",
+  delta: "d",
+  epsilon: "e",
+}
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers.keep_if {|digit| digit < 3}
+p numbers
+
+letters.keep_if {|greek, letter| letter < "d"}
+p letters
+
+## 3
+
+letters = {
+  alpha: "a",
+  beta: "b",
+  delta: "d",
+  epsilon: "e",
+}
+
+numbers = [1, 2, 3, 4, 5]
+
+newnumbers = numbers.select {|digit| digit.odd?}
+p numbers
+p newnumbers
+
+newletters = letters.select {|greek, letter| letter > "b"}
+p letters
+p newletters
+
+## 4
+
+letters = {
+  alpha: "a",
+  beta: "b",
+  delta: "d",
+  epsilon: "e",
+  alpha2: "a"
+}
+
+numbers = [1, 2, 3, 4, 5, 1]
+
+newnumbers2 = numbers.drop_while {|digit| digit < 3}
+p newnumbers2
+
+newletters2 = Hash[letters.drop_while {|greek, letter| letter < "d"}]
+p newletters2
 
