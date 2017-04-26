0cde11184363b8ad5b79d13ba9cc123af8f998e8
Jim Kyle <jim@jwkyle.com>
2017-04-25 19:13:53 -0500

    Add .each methods for R1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,18 +1,18 @@
 # Alan English and Jim Kyle
 
-def dog_info
-  puts "What's your dog's name?"
-  dog = gets.chomp
-  puts "What breed is he/she?"
-  breed = gets.chomp
-  yield(dog, breed)
-end
+# def dog_info
+#   puts "What's your dog's name?"
+#   dog = gets.chomp
+#   puts "What breed is he/she?"
+#   breed = gets.chomp
+#   yield(dog, breed)
+# end
 
-puts "Lets test our block!"
+# puts "Lets test our block!"
 
-dog_info { |dog, breed| puts "Wow, #{dog} is a lovely #{breed}!" }
+# dog_info { |dog, breed| puts "Wow, #{dog} is a lovely #{breed}!" }
 
-puts "Thanks for playing!"
+# puts "Thanks for playing!"
 
 
 #   puts "What's your dog's name?"
@@ -26,4 +26,43 @@ puts "Thanks for playing!"
 
 # dog_info( "#{dog}", "#{breed}")
 
- 
+
+## Release 1
+
+# Ninja Turtles
+turtles = {
+  Leonardo: "Blue",
+  Donatello: "Purple",
+  Raphael: "Red",
+  Michaelangelo: "Orange"
+}
+# Colors 
+colors = ["Black", "White", "Grey", "Pink"]
+
+
+# .each Array
+
+puts "Original data:"
+p colors
+
+colors.each do |hue|
+  p hue
+end
+
+puts "After .each call:"
+p colors
+
+# .each hash
+
+puts "Original Data:"
+p turtles
+
+turtles.each do |name , color|
+  puts "#{name} is #{color}"
+end
+
+puts "After .each call:"
+p turtles
+
+
+
