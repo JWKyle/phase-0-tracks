2b1b3a8241c0502f89bb0230028435fae826bed2
Jim Kyle <jim@jwkyle.com>
2017-04-27 12:28:38 -0500

    add comments and code for clarity, start hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,1 +1,41 @@
 #Jim Kyle
+# Comic book Data
+# Dc Comics 
+  # Superman
+    # Colors: Red and Blue
+    # Location
+    # Creators
+
+  # Batman
+    # Colors: Black, Yellow
+    # Location Gotham City
+    # Creators 
+
+  # Wonder Woman
+    # Colors Red, Blue, Gold
+    # Location
+    # Creators
+
+# Marvel Comics
+  # Spiderman
+    # Colors Red, Blue, Black
+  # Captain America
+    # Colors Red, White, Blue
+  # The Incredible Hulk
+    # Colors Green, Purple
+
+# Image Comics
+  # Spawn
+    # Colors Black White Red
+    # Location
+    # Creators
+
+
+
+comicbooks = {
+  dc_comics: {
+    superman: {
+      
+    }
+  }
+}
