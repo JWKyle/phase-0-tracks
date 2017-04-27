caf805f662360809113bc86ed515d0f2a4ffbdd9
Jim Kyle <jim@jwkyle.com>
2017-04-26 20:37:17 -0500

    add data set

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,2 +1,29 @@
 # Jim Kyle
-# 
+# Interior Designer Client Sheet
+
+# Plan
+# Create hash housing information from client
+  #Includes: Client first name, Client Last name, Client Age, number of children
+    # Decor theme, Desired/favorite color, pet resistant?, Other notes"
+#Prompt user
+#Print initial Hash
+#Give option to change info:
+  #if client enters name of key, ask for and update new value
+  #if client enters "none", exit changes
+#Print Edited hash
+
+## Data Set ##
+
+clientinfo = {
+  fname: "",
+  lname: "",
+  age: "",
+  numchild: "",
+  decor: "",
+  color: "",
+  pet: ""
+}
+
+
+
+
