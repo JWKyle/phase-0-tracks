7bf940e143378d0f99e839ccac9c3e901001f2eb
Jim Kyle <jim@jwkyle.com>
2017-04-26 20:52:52 -0500

    add asks, p init client info,

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -15,15 +15,36 @@
 ## Data Set ##
 
 clientinfo = {
-  fname: "",
-  lname: "",
-  age: "",
-  numchild: "",
-  decor: "",
-  color: "",
-  pet: ""
+  fname: nil,
+  lname: nil,
+  age: nil,
+  numchild: nil,
+  decor: nil,
+  color: nil,
+  pet: nil,
+  notes: nil
 }
 
+puts "What's your first name?"
+clientinfo[:fname] = gets.chomp
+puts "What's your last name?"
+clientinfo[:lname] = gets.chomp
+puts "What's your age?"
+clientinfo[:age] = gets.chomp.to_i
+puts "How many children do you have?"
+clientinfo[:numchild] = gets.chomp.to_i
+puts "What kind of decor are you looking for?"
+clientinfo[:decor] = gets.chomp
+puts "What is your favorite color?"
+clientinfo[:color] = gets.chomp
+puts "Do you need your home to be... uhhh... pet resistant?"
+clientinfo[:pet] = gets.chomp
+puts "Do you have anything to add?"
+clientinfo[:notes] = gets.chomp
+
+puts "Thanks!  Please review your information for accuracy:"
+puts " "
 
 
+p clientinfo
 
