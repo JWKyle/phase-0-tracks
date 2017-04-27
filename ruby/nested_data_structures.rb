11e1a1a8916919e670fa50f19fc295e6ce100f2e
Jim Kyle <jim@jwkyle.com>
2017-04-27 14:20:54 -0500

    completed pseudocode and data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -3,39 +3,59 @@
 # Dc Comics 
   # Superman
     # Colors: Red and Blue
-    # Location
+    # Location: Metropolis
     # Creators
+      # Writer: Jerry Siegel
+      # Artist: Joe Shuster
 
   # Batman
     # Colors: Black, Yellow
-    # Location Gotham City
-    # Creators 
+    # Location: Gotham City
+    # Creators: 
+      # Writer: Bill Finger
+      # Artist: Bob Kane
 
   # Wonder Woman
     # Colors Red, Blue, Gold
-    # Location
+    # Location: Halls of Justice
     # Creators
+      # Writer: William Moulton Marston
+      # Artist: Harry G. Peter
 
 # Marvel Comics
-  # Spiderman
+  # Spider-man
     # Colors Red, Blue, Black
+    # Location: New York
+    # Creators:
+      # Writer: Stan Lee
+      # Artist: Steve Ditko
+
   # Captain America
     # Colors Red, White, Blue
-  # The Incredible Hulk
+    # Location: New York
+    # Creators:
+      # Writer: Joe Simon
+      # Artist: Jack Kirby
+
+  # The Hulk
     # Colors Green, Purple
+    # Location: Unknown
+    # Creators:
+      # Writer: Stan Lee
+      # Artist: Jack Kirby
 
 # Image Comics
   # Spawn
     # Colors Black White Red
     # Location
-    # Creators
+    # Creator: Todd McFarlane
 
 
 
 comicbooks = {
   dc_comics: {
     superman: {
-      
+
     }
   }
 }
