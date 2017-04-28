02d15fb917e50794b0514fc02fe0b4a0430762f2
Jim Kyle <jim@jwkyle.com>
2017-04-27 22:01:28 -0500

    add pseudocode

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,1 +1,13 @@
 #Jim Kyle
+#Create a method
+  #Takes a string for first and last name
+    #Gets chomp 
+    # turn each string to an array, 
+  #change first name with last name
+    # indexed by character
+    #.next each letter, either by vowel or consonant array
+  #Shift vowels down the alphabet 1 space (a->e)
+  #Shift consonants to the next consonant
+  # abcdefghijklmnopqrstuvwxyz = alphabet
+  # vowels = alphabet - aeiou 
+  # 2 arrays needed: [a, e, i, o, u] and alphabet - vowels
