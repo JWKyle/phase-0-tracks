ea1668b1b20b2f2b8be06642713548465b1a7adf
Jim Kyle <jim@jwkyle.com>
2017-04-29 19:01:17 -0500

    add answers for r2, still needs pseudo and code

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,4 +1,5 @@
 ## Danny Marks and Jim Kyle
+###   0   ###
 
 def search_array(array, input)
   index = 0
@@ -13,7 +14,7 @@ def search_array(array, input)
 end
 search_array([1,2,3,4,234,78765,21], 45)
 
-
+####   1   ###
 
 
 def fibonacci( n )
@@ -27,3 +28,19 @@ length.legt
 
 
 ( #{index} -1)
+
+  ### 2 ###
+  # 1. For this exercise, we chose bubble search.
+
+  # 2. Personally, visualizations helped me grasp the concept, along with 
+  # looking at the formula.  I intend to keep this in mind as a search for more 
+  # challenges via google.  Certain texts and pages with walkthroughs helped
+  # as well.
+
+  # 3. Initially, for a split second, it was a little overwhelming.  But after
+  # taking a beat, I became more excited about what I was reading.  Typically, I
+  # learn a lot more quickly and efficiently when I'm that kind of excited 
+  # When I'm overwhelmed, I tend to mentally feel a little foggy, and I tend to tr
+  # trudge along.
+
+  # 4.  
