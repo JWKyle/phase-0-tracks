b4b62f210d3480ea21e77da00ff8ab72cb87711e
Jim Kyle <jim@jwkyle.com>
2017-05-01 16:56:22 -0500

    retooled code, now works properly, need to join strings

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -18,56 +18,108 @@
   #.first
   #.rotate
 
-  
+  # fullalphabet = "abcdefghijklmnopqrstuvwxyz"
+# consonants = $fullalphabet.delete 'aeiou'
+# alpha = ['a', 'b', 'c', 'd', 'e', 'u', 'z']
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# consonants = fullalphabet.delete 'aeiou'
+  def namecoder
+    original_name = []
+    
+    puts "Whats your first name?"
+    fname = gets.chomp.split('')
+    puts "What's your last name?"
+    lname = gets.chomp.split('')
+    fullname = [fname," ", lname].flatten
+    p fullname
+    
+    fullalphabet = "abcdefghijklmnopqrstuvwxyz"
+    consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+    alpha = ['a', 'b', 'c', 'd', 'e', 'u', 'z']
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    # vowel_code(original_name)
+      vowels = ['a', 'e', 'i', 'o', 'u']
+    fn_vowel = fullname.map do |vowel|
+      if vowels.include?(vowel)
+        vowels.rotate(1)[vowels.index(vowel)]
+      else
+        vowel
+      end
+    end
+  p fn_vowel  
+    # name_vowels_changed.join
+ 
+    # def cons_code(name_vowels_changed)
+      # name_vowels_changed = name_vowels_changed.split('')
+    name_both_changed = fn_vowel.map do |cons|
+      if consonants.include?(cons)
+        consonants.rotate(1)[consonants.index(cons)]
+      else
+        cons
+      end
+    end
+    p name_both_changed
+    
+  end
+  namecoder
+
+
+
 
 
 # # end  
 # def secret_name(fullname)
 
-$fullalphabet = "abcdefghijklmnopqrstuvwxyz"
-$consonants = fullalphabet.delete 'aeiou'
+# $fullalphabet = "abcdefghijklmnopqrstuvwxyz"
+# $consonants = fullalphabet.delete 'aeiou'
 
 
 
 
-$alpha = ['a', 'b', 'c', 'd', 'e', 'u', 'z']
-$vowels = ['a', 'e', 'i', 'o', 'u']
-# consonants = fullalphabet.delete 'aeiou'
+# $alpha = ['a', 'b', 'c', 'd', 'e', 'u', 'z']
+# $vowels = ['a', 'e', 'i', 'o', 'u']
+# # consonants = fullalphabet.delete 'aeiou'
 
-  puts "Whats your first name?"
-  fname = gets.chomp.downcase!
-  puts "What's your last name?"
-  lname = gets.chomp.downcase!
+#   puts "Whats your first name?"
+#   fname = gets.chomp.downcase!
+#   puts "What's your last name?"
+#   lname = gets.chomp.downcase!
+#   original_name = [fname, lname]
 
   
-  def vowel_code(letter)
-  valpha = $vowels.map do |vowel|
-    if $vowels.include?(vowel)
-      $vowels.rotate(1)[$vowels.index(vowel)]
-    else
-      vowel
-    end
-  end
-end
-  def cons_code(letter)
-  scons = $consonants.map do |cons|
-    if $consonants.include?(cons)
-      $consonants.rotate(1)[$consonants.index(cons)]
-    else
-      cons
-    end
-  end
- end 
+#   def vowel_code(original_name)
+#     original_name = original_name.split('')
+#   name_vowels_changed = original_name.map do |vowel|
+#     if $vowels.include?(vowel)
+#       $vowels.rotate(1)[$vowels.index(vowel)]
+#     else
+#       vowel
+#     end
+#   end
+#   name_vowels_changed.join
+# end
+#   def cons_code(name_vowels_changed)
+#     name_vowels_changed = name_vowels_changed.split('')
+#   name_both_changed = name_vowels_changed.map do |cons|
+#     if $consonants.include?(cons)
+#       $consonants.rotate(1)[$consonants.index(cons)]
+#     else
+#       cons
+#     end
+#   end
+#   name_both_changed.join
+#  end 
 
 
-   fullname =[fname, lname].reverse!
-  p fullname
-  # puts "Whats your first name?"
-  # fname = gets.chomp
-  # puts "What's your last name?"
-  # lname = gets.chomp
-  # fullname =[fname, lname].reverse!
-  # p fullname
+#    fullname =[fname, lname].reverse!
+#   p fullname
+#   # puts "Whats your first name?"
+#   # fname = gets.chomp
+#   # puts "What's your last name?"
+#   # lname = gets.chomp
+#   # fullname =[fname, lname].reverse!
+#   # p fullname
 
 
 
