f2e3fab623e7863b792721f1c00a512c20d1257c
Jim Kyle <jim@jwkyle.com>
2017-05-03 10:58:44 -0500

    add and test multiple dog tricks

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -6,5 +6,36 @@ class Puppy
     toy
   end
 
-end
+  def speak(number)
+    number.times {puts "Woof!"}
+  end  
 
+  def roll_over
+    puts "*rolls over*"
+  end  
+
+  def dog_years(human_years)
+    dog_years = human_years * 7
+    puts "The dog is #{dog_years} in dog years"
+    dog_years
+  end  
+
+  def shake(paw)
+    if paw == "right"
+      puts "*shakes your hand with it's right paw*"
+    elsif paw == "left"
+      puts "*shakes your hand with it's left paw*"
+    else
+      puts "*tilts head in confusion*"
+    end  
+  end    
+      
+end
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(3)
+puppy.roll_over
+puppy.dog_years(7)
+puppy.shake("right")
+puppy.shake("left")
+puppy.shake("blah")
