76e8dae25a3c6312f7b7246e77efb2660325e0eb
Jim Kyle <jim@jwkyle.com>
2017-05-05 11:54:36 -0500

    created mix in shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,41 @@
 ## Jim Kyle
 
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + "  :("
-  end  
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + "  :("
+#   end  
 
-  def self.yelling_happily(words)
-    words + "!!!" + " YIPPPEEEE!!! :)"
+#   def self.yelling_happily(words)
+#     words + "!!!" + " YIPPPEEEE!!! :)"
+#   end  
+# end
+
+# p Shout.yell_angrily("Game over, man!  GAME OVER")
+# p Shout.yelling_happily("Game over, man!  GAME OVER")
+
+module Shout
+  def drive(number, measure)
+    puts "Wow!  That was a long drive!  That's got to be #{number} #{measure}!"
+  end
+  
+  def car 
+    puts "Wait... is that my car?"  
   end  
 end
 
-p Shout.yell_angrily("Game over, man!  GAME OVER")
-p Shout.yelling_happily("Game over, man!  GAME OVER")
+class Golfer
+  include Shout
+end
+
+class Grandparent
+  include Shout
+end
+
+golfer = Golfer.new
+golfer.drive(300, "yards")
+golfer.car
+
+grandparent = Grandparent.new
+grandparent.drive(75, "miles")
+grandparent.car
 
