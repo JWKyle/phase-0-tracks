5c049e968426126290974276f6e252aef7022913
Jim Kyle <jim@jwkyle.com>
2017-05-06 22:15:14 -0500

    worked on guess method, added repeat

--- ruby/wordgame/snipe_game.rb
+++ ruby/wordgame/snipe_game.rb
@@ -1,17 +1,29 @@
 ## Jim Kyle
 ## 6.7 Game ##
 class Game
-  attr_reader :word
+  attr_reader :word, :number
   attr_accessor :guesses
   
   def initialize(secret_word)
+    @number = secret_word.length * 2
+    p @number
     @word = secret_word.split('')
     p @word
-    @guesses = []
-    p @guesses
+    @guessed = []
+    p @guessed
   end
 
   def guess(letter)
+    @guessed.each do |repeat|
+      if repeat == letter
+        puts "You've already used that letter.  Try again."
+      else
+        @guessed << letter
+      end
+    @word.each do |guess|
+      if guess == letter
+        
+
 
     
   end
