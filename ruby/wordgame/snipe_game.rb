6e5d7449a41ebeb0c1d9dac917c415327376ceea
Jim Kyle <jim@jwkyle.com>
2017-05-06 21:26:26 -0500

    moved to new file due to file issues.

---
+++ ruby/wordgame/snipe_game.rb
@@ -0,0 +1,21 @@
+## Jim Kyle
+## 6.7 Game
+class Game
+  attr_reader :word
+  attr_accessor :guesses
+  
+  def initialize(secret_word)
+    @word = secret_word.split('')
+    p @word
+    @guesses = []
+    p @guesses
+  end
+
+  def guess(letter)
+
+    
+  end
+end
+
+game = Game.new("test")
+
