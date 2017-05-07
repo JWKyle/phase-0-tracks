540ea8f660844ce44d3ddafe7633b50b27fe49a2
Jim Kyle <jim@jwkyle.com>
2017-05-07 11:48:55 -0500

    add guess method

--- ruby/wordgame/snipe_game.rb
+++ ruby/wordgame/snipe_game.rb
@@ -1,7 +1,7 @@
 ## Jim Kyle
 ## 6.7 Game ##
 class Game
-  attr_reader :word, :number
+  attr_reader :word, :number, :solution
   attr_accessor :guesses
   
   def initialize(secret_word)
@@ -11,23 +11,37 @@ class Game
     p @word
     @guessed = []
     p @guessed
+    @solution = []
+    p @solution
   end
 
-  def guess(letter)
+  def reguess(letter)
     @guessed.each do |repeat|
       if repeat == letter
         puts "You've already used that letter.  Try again."
       else
         @guessed << letter
       end
+    end 
+  puts "You have guessed the following letters: #{@guessed}" 
+  end
+
+  def guess(letter)
     @word.each do |guess|
       if guess == letter
-        
-
-
-    
+        @solution << letter
+      # else
+        # @solution << letter
+      end
+      # return @solution
+    end  
+    p @solution 
   end
+
+
 end
 
 game = Game.new("test")
-
+game.guess("e")
+game.guess("t")
+game.reguess("t")
