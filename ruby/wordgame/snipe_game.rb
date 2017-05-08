82de945230cb938bf655aa59e3f9541b537de092
Jim Kyle <jim@jwkyle.com>
2017-05-07 20:44:19 -0500

    Added code to guess/reguess

--- ruby/wordgame/snipe_game.rb
+++ ruby/wordgame/snipe_game.rb
@@ -1,47 +1,59 @@
 ## Jim Kyle
 ## 6.7 Game ##
 class Game
-  attr_reader :word, :number, :solution
+  attr_reader :word, :number, :solution, :guessed
   attr_accessor :guesses
   
   def initialize(secret_word)
     @number = secret_word.length * 2
-    p @number
+    # p @number
     @word = secret_word.split('')
-    p @word
+    # p @word
     @guessed = []
-    p @guessed
+    # p @guessed
     @solution = []
-    p @solution
+    # p @solution
+    @count = 0
+    # p @count
+    @remaining_turns = @number - @count
+    # p @remaining_turns
   end
 
-  def reguess(letter)
-    @guessed.each do |repeat|
-      if repeat == letter
-        puts "You've already used that letter.  Try again."
-      else
-        @guessed << letter
-      end
-    end 
-  puts "You have guessed the following letters: #{@guessed}" 
-  end
+  # def reguess(letter)
+  #   @guessed.each do |repeat|
+  #     if repeat == letter
+  #       puts "You've already used that letter.  Try again."
+  #     else
+  #       @guessed << letter
+  #     end
+  #   end 
+  # puts "You have guessed the following letters: #{@guessed}" 
+  # end
 
   def guess(letter)
+      if @guessed.include?(letter) do
+        puts "You've guessed that letter already.  Please try again."
+        break
+        @guessed << letter
+      end
+     p @guessed 
+    end
+
     @word.each do |guess|
       if guess == letter
         @solution << letter
-      # else
+      else
         # @solution << letter
       end
-      # return @solution
     end  
     p @solution 
+    p @guessed
   end
-
-
 end
 
 game = Game.new("test")
 game.guess("e")
-game.guess("t")
-game.reguess("t")
+game.guess("d")
+game.guess("s")
+game.guess("b")
+# game.reguess("t")
