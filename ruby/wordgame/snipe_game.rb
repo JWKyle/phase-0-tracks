a42ad8ce5b8db51ea75550394f46d9d54ed4042c
Jim Kyle <jim@jwkyle.com>
2017-05-07 23:16:15 -0500

    completed array word correlation

--- ruby/wordgame/snipe_game.rb
+++ ruby/wordgame/snipe_game.rb
@@ -7,8 +7,8 @@ class Game
   def initialize(secret_word)
     @number = secret_word.length * 2
     # p @number
-    @word = secret_word.split('')
-    # p @word
+    @word = secret_word.downcase.chars
+    p @word
     @guessed = []
     # p @guessed
     @solution = []
@@ -16,7 +16,9 @@ class Game
     @count = 0
     # p @count
     @remaining_turns = @number - @count
-    # p @remaining_turns
+    p @remaining_turns
+    @working_word = "-" * secret_word.length
+    p @working_word
   end
 
   # def reguess(letter)
@@ -32,29 +34,29 @@ class Game
 
 
   def guess(letter)
-      
-      if @guessed.include?(letter)
-        puts "You've guessed that letter already.  Please try again."
-      else
-        @guessed << letter
-        puts "You guessed #{letter}"
-      end
-    end
-    @word.each do |guess|
-      if guess == letter
-        @solution << letter
-      else
-        # @solution << letter
+    @guessed << letter.downcase
+    @word.each_with_index do |guess, index|
+      if guess == letter.downcase
+        @working_word[index] = letter.downcase
       end
     end 
     p @solution 
     p @guessed
+    p @working_word
   end
 end
 
 game = Game.new("test")
-game.guess("e")
-game.guess("d")
 game.guess("s")
+game.guess("d")
+game.guess("t")
 game.guess("b")
 # game.reguess("t")
+
+  #   if @guessed.include?(letter)
+    #     puts "You've guessed that letter already.  Please try again."
+    #   else
+    #     @guessed << letter
+    #     puts "You guessed #{letter}"
+    #   end
+    # end
