216dc8ced3a7b8adfa5ef627013ee7c5c4d9a7e2
Jim Kyle <jim@jwkyle.com>
2017-05-12 20:32:04 -0500

    Add psuedocode to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,16 @@
+// Reverse in JS
+
+// Create a function that takes a string as a parameter, and reverses the string.
+  // Since JS takes .length, we can use that to create space for new word?
+  // We can use a loop and 'i' instead of .each and a block
+    // First, we create a method that takes a string as a parameter
+    // Next we take the first letter of the string, and add it to the front of a new array
+    // Next we take the new first letter, and add it to the front position of the new array
+      // so on and so forth
+    // Convert back to string  
+    // We print the final string with a console.log
+      // do we turn this into an array for spacing?  would shift work?
+
+
+
+
