0930545ef0065e4e188cbf2b473258bd96479957
Jim Kyle <jim@jwkyle.com>
2017-05-14 18:14:39 -0500

    add pseudocode and array, outlining function

---
+++ js/algos.js
@@ -0,0 +1,32 @@
+// 7.3 Pseudocode
+//. Write a function that takes an array, then returns the longest word in the array.
+  // create an array of words with varying lengths
+  // determine the length of each word
+    // will need to use a .length per array item
+  // return the word with the most letters
+
+
+
+
+
+
+// Function code
+longwords = ["apple", "banana", "pear"]
+
+function longestw() { 
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// driver code
+ console.log(longestw);
+
