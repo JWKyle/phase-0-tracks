7f4a5cf5974de8a2579d94604c7ab2a7f2bba93d
Jim Kyle <jim@jwkyle.com>
2017-05-14 20:48:12 -0500

    pseudo and add code to combine two arrays into an object.  tested.

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,5 +10,18 @@ var names = ["Ed", "Al", "Bo", "Joe"];
 colors.push("Black");
 names.push("Bob");
 
-console.log(colors);
-console.log(names);
+//console.log(colors);
+// console.log(names);
+
+// R1 
+// Key = names, Values = colors
+// object is going to look like horses = {names: colors}
+// loop through index, each index number would push the name/color combination into the object
+
+var horses = {};
+
+for (var i=0; i < colors.length; i++){
+
+horses[names[i]] = colors[i] };
+
+//console.log(horses);
