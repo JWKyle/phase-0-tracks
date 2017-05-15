9086093541feaa37b94ab360a4fcaccebb1e8aa0
Jim Kyle <jim@jwkyle.com>
2017-05-14 20:13:21 -0500

    add pseudocode and code for arrays

---
+++ js/data_structures.js
@@ -0,0 +1,14 @@
+// 7.2 MPS
+
+// Build an array with 4 colors 
+// Build an array with 4 horse names
+
+
+var colors = ["Blue", "Purple", "Green", "Yellow"];
+var names = ["Ed", "Al", "Bo", "Joe"];
+
+colors.push("Black");
+names.push("Bob");
+
+console.log(colors);
+console.log(names);
